5c7990f5245dd4a4a4db8acfdab05d9889cec89a
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-29 15:16:00 -0400

    fixed somewhat the axios request and setup the react dom render so know we can create multiple links

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import {useState,useEffect} from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
-// useEffect(() => {
-//   axios.get('/dogs').then(data =>{
-//     console.log(data)
-//   })
-// },[])
+  // const getData = async () => {
+  //   const {data} = await axios.get("http://localhost:8080/dogs")
+  //   console.log(data)
+  // };
+  // useEffect(()=> {
+  //   getData()
+  // },[])
+  useEffect(() => {
 
+  axios.get('http://localhost:8080/dogs')
+  .then(response =>{
+  console.log(response.data)
+  })
+  },[])
+
+  
   return (
-    // <div className="App">
-    //   <h1>Cats App</h1>
-    // </div>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+  <Router>
+    <h1>Acceses the React DOM Render</h1>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+)
 }
 
 export function Home() {
--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ const app = express();
 // middleware setup
 app.use(morgan(ENVIROMENT));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {   res.header("Access-Control-Allow-Origin", "*");   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");   next(); });
+
 const catsRoutes = require("./routes/catsRoutes")
 
 app.use("/cats",catsRoutes)
