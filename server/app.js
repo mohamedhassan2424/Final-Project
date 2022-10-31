7370fe5fcbcd7017a9e7ff72fc88a43f3bce00dd
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-31 18:20:55 -0400

    adding the if statments to check the database when we need to log-in

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ const {ENVIROMENT, PORT} = process.env;
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 const app = express();
 const { Pool } = require('pg');
 // middleware setup
 app.use(morgan(ENVIROMENT));
 app.use(bodyParser.json());
+app.use(cors());
 const pool = new Pool({
     port:'5432',
     user: 'labber',
@@ -48,6 +49,22 @@ app.post("/reigister", (req,res)=>{
     const password = req.body.password
 pool.query("INSERT INTO users (firstName, lastName, email, password) VALUES ($1,$2,$3,$4)",[firstName, lastName, email, password])
 }) 
+app.get("/login", (req,res)=>{
+
+    const emalFromDatabase = req.body.email;
+    const passwordFromDatabase = req.body.password;
+    pool.query("SELECT * FROM  users WHERE email = $1 AND password = $2 ",[emalFromDatabase,passwordFromDatabase])
+    .then((response)=>{
+        if(response) {
+        res.send(response)
+        } else {
+            res.send("Incorrect UserName and Password")
+        }
+    })
+    .catch((error)=>{
+        console.log(error.message)
+    })
+})
 
 app.get("/content",(req,res)=>{
     return pool.query(`SELECT * FROM users;`)
