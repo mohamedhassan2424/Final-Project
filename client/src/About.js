007f944295cdd18043cf57a21eb5ec557313de64
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 10:07:06 -0400

    creating the html and the css in react for the homepage

--- client/src/About.js
+++ client/src/About.js
@@ -5,13 +5,27 @@ function About(props) {
 
 
     return (
-        <div>
-            <div class = "header">
-
-            
-            <nav></nav>
-            </div>
+        <div className="navContainer">
+    {/* <h1>Hello world Mohamed Hassan</h1> */}
+    <div className="navbar">
+        <div className ="logo">
+                <img className="logoImage"src="https://media.istockphoto.com/vectors/vegetables-on-shopping-cart-trolley-grocery-logo-icon-design-vector-vector-id1205419959?k=20&m=1205419959&s=612x612&w=0&h=F4gyp5wuFkCaZr00OQS8KPCSE1_4pHmFiOIM2TQlOPI="  />
         </div>
+        <nav className ="navlistItems">
+            <ul>
+                <li><a href="'">Home Page</a></li>
+                <li><a href="'">Stores</a></li>
+                <li><a href="'">Products</a></li>
+                <li><a href="'">Products</a></li>
+                <li><a href="'">Account</a></li>
+                <li><a href="'">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div className="eachrowas">
+
+    </div>
+</div>
     );
 }
 
