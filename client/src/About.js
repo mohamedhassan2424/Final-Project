42e1b763c314e6f7f635dc79df8bb6bc90843aa6
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 14:48:28 -0400

    adding the content for the information for th stores

--- client/src/About.js
+++ client/src/About.js
@@ -11,10 +11,12 @@ function About(props) {
                     {/* <h1>Hello world Mohamed Hassan</h1> */}
                     <div className="navbar">
                         <div className="logo">
+                            <span className="logoName">LITE CART</span>
                             <img className="logoImage" src="https://media.istockphoto.com/vectors/vegetables-on-shopping-cart-trolley-grocery-logo-icon-design-vector-vector-id1205419959?k=20&m=1205419959&s=612x612&w=0&h=F4gyp5wuFkCaZr00OQS8KPCSE1_4pHmFiOIM2TQlOPI=" />
                         </div>
                         <nav className="navlistItems">
                             <ul>
+                                
                                 <li><a href="'">Home Page</a></li>
                                 <li><a href="'">Stores</a></li>
                                 <li><a href="'">Products</a></li>
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,16 +1,46 @@
 import React from "react"
 
 function Home(props) {
-    return (
-    
-    <div>
-<h1>Connected to the Home page</h1>
+  return (
 
+    <div>
+      <h1>LITE-CART</h1>
+      <div>
+        <nav className="navlistItems">
+          <ul>
+            <li><a href="'">Home Page</a></li>
+            <li><a href="'">Stores</a></li>
+            <li><a href="'">Products</a></li>
+            <li><a href="'">Products</a></li>
+            <li><a href="'">Account</a></li>
+            <li><a href="'">Contact</a></li>
+          </ul>
+          <img className="cartIcon" alt="Image cart" src="../Images/5a364b6d2c5557.7578312615135076931816 (1).png"></img>
+        </nav>
+      </div>
+<div>
+  <h1>Recentely Visted</h1>
+
+</div>
+
+<div>
+<h3>All Stores in Toronto</h3>
+<div className="mainClassInStores">
+<div className="logoImage"></div>
+<div className="contentOfStore">
+<p> NEW</p>
+<p> METRO</p>
+<p> DELEIVER WITHTIN AN HOUR</p>
+<p> In-stores prices</p>
+</div>
+</div>
+
+
+</div>
     </div>
 
 
-    );
-  }
+  );
+}
 
-  export default Home;
-  
+export default Home;
