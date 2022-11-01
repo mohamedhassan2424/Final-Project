b195c3699ed9ae08f241cd78501be3f3d0110fba
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 19:23:08 -0400

    making alot of changes to the css to the store page

--- client/src/Home.js
+++ client/src/Home.js
@@ -19,12 +19,24 @@ function Home(props) {
         </nav>
       </div>
 <div>
-  <h1>Recentely Visted</h1>
+  <h3  className ="storeTitle" >Recentely Visted</h3>
+  <div className="sampleDataContainer">
 
+<div className="logoImages">
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2138/19b444c0-481d-41cf-b017-4d1666e81006.png" / >
+
+  
+</div>
+<div className="logoImages">
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/227/0b338c85-d46f-4a49-a48c-9dd281ccfdcf.png" / >
+
+  
+</div>
+</div>
 </div>
 
 <div>
-<h3>All Stores in Toronto</h3>
+<h3 className ="storeTitle">All Stores in Toronto</h3>
 <ul className ="storeContainer">
   <li className="eachStore">
   <div className="mainClassInStores">
@@ -154,7 +166,7 @@ function Home(props) {
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Pu</p>
+<p> Pusateri's</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -170,13 +182,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/551/0231a932-a563-49ca-8da7-532cb5cdfeb6.png"/ >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Staples</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -190,13 +202,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/610/2a87c25b-5a28-4cb8-b1bc-cd5c57d3dae6.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> M&M Food Market</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -210,13 +222,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/635/8731ed2a-42a0-46a1-baf3-07713a8c6475.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Bulk Barn</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -230,13 +242,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1256/7e9d582a-bf60-4a4d-8989-8705c0f39e13.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Eataly</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -250,13 +262,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1427/55aae31c-6c4e-4c30-a6fd-b7fd4018e85c.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Organic Garage</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -270,13 +282,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1509/77eca83d-0d98-410d-8047-6f205fe0623d.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Indigo</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -290,13 +302,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1555/27f6ac04-0756-4fcc-ab3e-06bf97b9e3a2.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Sephora Canada</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -310,13 +322,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1603/7d6204e2-859f-499d-8236-e15aaf252007.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Dollarama</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -330,13 +342,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1634/84f40c52-a22b-4a89-a986-b82e09cb7455.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p>Mastermind Toys</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -350,13 +362,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1646/1ba1f48f-c935-42ae-9129-d3979a38a395.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> LCBO</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -370,13 +382,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1702/c748f770-ee1f-494d-9f6b-606a3d276429.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> No Frills</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -390,13 +402,33 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1721/91622d5c-9f13-4f6d-9d4d-eaef6367c947.png" / >
+
+
+</div>
+<div className="contentOfStore">
+<p className ="newText"> McEwan Fine Foods</p>
+<p> </p>
+<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
+<p className="storePriceText"> In-stores prices</p>
+</div>
+</div>
+
+
+
+</li>
+
+<li className="eachStore">
+
+<div className="mainClassInStores">
+<div className="logoImage">
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1765/4c39dd8f-9554-41b9-86f8-6e13b6525da2.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> The Body Shop Canada</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -410,13 +442,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1898/f7672f30-e00b-490d-8abd-ca339b4a9914.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Healthy Planet</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -430,13 +462,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1928/9afc1db8-0780-4246-91c5-8f4d4de1fc33.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Micheal's Canada</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -450,13 +482,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/1992/98097d7f-1969-40fd-8f4d-b8d5c6d28402.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Popeye's Supplements</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -470,13 +502,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2044/15a00d4e-9520-47e6-af90-1c10d1ee76e5.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Grande Cheese Markets</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -490,13 +522,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2047/99c9f3cb-3660-4d33-91fd-a590f92fa445.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Super Natural Market</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -510,13 +542,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2081/82c88696-2779-4555-bd8c-0c69477af354.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Dolce & Gouramando</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -530,13 +562,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2127/a8fc4dcb-5fdf-4bec-9b3c-cff8fa8ec7ac.jpg" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Greenhouse</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -550,13 +582,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2147/4d6986c6-3328-4d48-95d9-760d4e4c578d.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> XMarket</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -570,13 +602,13 @@ function Home(props) {
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2176/ca7efb30-bbab-437c-877f-7dd37fe3c652.png" / >
 
 
 </div>
 <div className="contentOfStore">
 <p className ="newText"> NEW</p>
-<p> Loblaws</p>
+<p> Harvest Wagon</p>
 <p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
 <p className="storePriceText"> In-stores prices</p>
 </div>
@@ -586,71 +618,13 @@ function Home(props) {
 
 </li>
 
-<li className="eachStore">
 
-<div className="mainClassInStores">
-<div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
-
-
-</div>
-<div className="contentOfStore">
-<p className ="newText"> NEW</p>
-<p> Loblaws</p>
-<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
-<p className="storePriceText"> In-stores prices</p>
-</div>
-</div>
-
-
-
-</li>
 
 <li className="eachStore">
 
 <div className="mainClassInStores">
 <div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
-
-
-</div>
-<div className="contentOfStore">
-<p className ="newText"> NEW</p>
-<p> Loblaws</p>
-<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
-<p className="storePriceText"> In-stores prices</p>
-</div>
-</div>
-
-
-
-</li>
-
-<li className="eachStore">
-
-<div className="mainClassInStores">
-<div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
-
-
-</div>
-<div className="contentOfStore">
-<p className ="newText"> NEW</p>
-<p> Loblaws</p>
-<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
-<p className="storePriceText"> In-stores prices</p>
-</div>
-</div>
-
-
-
-</li>
-
-<li className="eachStore">
-
-<div className="mainClassInStores">
-<div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
+<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/2177/2d8b7943-5619-401b-bc3d-504990263af7.jpg" / >
 
 
 </div>
@@ -668,45 +642,7 @@ function Home(props) {
 </li>
 
 
-<li className="eachStore">
 
-<div className="mainClassInStores">
-<div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
-
-
-</div>
-<div className="contentOfStore">
-<p className ="newText"> NEW</p>
-<p> Loblaws</p>
-<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
-<p className="storePriceText"> In-stores prices</p>
-</div>
-</div>
-
-
-
-</li>
-
-<li className="eachStore">
-
-<div className="mainClassInStores">
-<div className="logoImage">
-<img src="https://www.instacart.com/image-server/72x/www.instacart.com/assets/domains/warehouse/logo/311/e206e4c7-3dd9-48b1-8dc2-f4ce50df7414.png" / >
-
-
-</div>
-<div className="contentOfStore">
-<p className ="newText"> NEW</p>
-<p> Loblaws</p>
-<p className ="deliveryText"> DELEIVER WITHTIN AN HOUR</p>
-<p className="storePriceText"> In-stores prices</p>
-</div>
-</div>
-
-
-
-</li>
   
 
 </ul>
