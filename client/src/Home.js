199017134b3a3538bfd2bc051dc107e7161d7031
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 14:30:54 -0400

    created the stores page

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,7 +2,13 @@ import React from "react"
 
 function Home(props) {
     return (
-    <h1>Connected to the Home page</h1>
+    
+    <div>
+<h1>Connected to the Home page</h1>
+
+    </div>
+
+
     );
   }
 
