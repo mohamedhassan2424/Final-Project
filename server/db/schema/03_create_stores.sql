2da951ba16118fa1e126839e7f1e6dce6d5c4f50
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 16:14:36 -0400

    changes made to the database

--- server/db/schema/03_create_stores.sql
+++ server/db/schema/03_create_stores.sql
@@ -3,5 +3,9 @@ CREATE TABLE stores (
   id SERIAL PRIMARY KEY,
   -- products_id integer REFERENCES products(id) ON DELETE CASCADE NOT NULL,
   store_name VARCHAR(255) NOT NULL,
+-- owner of the store, its id
   rating VARCHAR(255) NOT NULL
+  -- created_at TIMESTAMP FOR ALL THE TAB
+  -- description of the store
+  -- deactive column(true or false) for the store
 );
