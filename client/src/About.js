392f33ef1eb6fbfbf60acc14417e58245d73a9dc
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 10:45:27 -0400

    added the a button and as well the background color for the header for the main-page

--- client/src/About.js
+++ client/src/About.js
@@ -5,6 +5,7 @@ function About(props) {
 
 
     return (
+        <header className= "header">
         <div className="navContainer">
     {/* <h1>Hello world Mohamed Hassan</h1> */}
     <div className="navbar">
@@ -27,13 +28,14 @@ function About(props) {
                 <h1>Look no Further! We got Everything for you.</h1>
                 <h1>Grocery Shoppping has never been this fun.</h1>
                 <p>To deliver an exceptional shopping experience by offering the best service, value, quality, and freshest products while being good stewards of our environment and giving back to the communities we serve.</p>
-                <a href="" className="firstbutton">CHECK IT OUT</a>
+                <a href="" className="firstbutton">CHECK IT OUT &#x2192;</a>
         </div>
         <div className="eachcloumn">
         <img className="secoundlogoImage" src="https://media.istockphoto.com/photos/shopping-basket-with-fresh-food-grocery-supermarket-food-and-eats-picture-id1216828053?k=20&m=1216828053&s=612x612&w=0&h=mMGSO8bG8aN0gP4wyXC17WDIcf9zcqIxBd-WZto-yeg="  />
         </div>
     </div>
 </div>
+</header>
     );
 }
 
