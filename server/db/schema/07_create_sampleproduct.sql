bbeecbe59d3743713bd0fd94742e0a67f94bbb73
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-02 09:11:10 -0400

    adding data manual in the product page but categories it by adding in a category_name

---
+++ server/db/schema/07_create_sampleproduct.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  -- store_id integer REFERENCES stores(id) ON DELETE CASCADE NOT NULL,
+  category_name_value VARCHAR(255) NOT NULL,
+  product_name VARCHAR(255) NOT NULL,
+  product_description VARCHAR(255) NOT NULL,
+  product_url VARCHAR(255) NOT NULL,
+  price FLOAT, 
+  sale_price FLOAT,
+  quantity INTEGER
+
+)
--- server/db/seeds/04_products.sql
+++ server/db/seeds/04_products.sql
@@ -1,25 +1,53 @@
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy', 'Nestlé Aero Milk Chocolate Bar 4 Pack 42 g','4 x 42g($2.67 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/051df510-051f-475a-afe7-47682b50381d/300x300.jpg' ,4.49,5.49,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Cheerios Honey Nut Cereal 430 g','430g($1.04 per 100g','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/e705341e-c803-4794-83f1-f30ccc6b9629/300x300.jpg' ,4.49,6.49,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Liberté Mediterranee Yogurt Blackberry 500 g','500g($0.80 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/96c1ec03-cef1-4824-8b61-32fcd55a03e8/300x300.jpg' ,4.49,6.49,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( 'Dairy', 'Wavy Lays Potato Chips Original 235 g','235g ($1.06 per 100g)','https://cdn-icons-png.flaticon.com/512/3600/3600488.png" /><h4 className="saleDescription' ,2.50,3.99,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( 'Dairy','Rainbow Trout Fillet','0.34kg typically ($26.44 per kilogram)','https://cdn-icons-png.flaticon.com/512/3600/3600488.png" /><h4 className="saleDescription' ,8.99,11.24,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  '  Compliments Frozen Juice Berry Punch 283 ml','283ml ($0.31 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/b9ed18b8-da8c-41da-b92e-97e483668b9d/300x300.jpg' ,0.88,1.19,5);
-INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
--- INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ( '','','' ,3.24234,5.23423,5);
 
 
---   category_name_value VARCHAR(255) NOT NULL,
---   product_name VARCHAR(255) NOT NULL,
---   product_description VARCHAR(255) NOT NULL,
---   product_url VARCHAR(255) NOT NULL,
---   price FLOAT, 
---   sale_price FLOAT,
---   quantity INTEGER
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Liberte Greek High In Protien Yogurt Mango 750 g','750g($1.00 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/8b045b18-aadc-4174-b7f2-300cc0e31c26/300x300.jpg' ,7.49,1.5,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy', 'Liberte Greek Yogurt Wild Blueberry 750 g','750g($1.00 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/ccdf95d5-8fa2-4157-a7a1-da4321932530/300x300.jpg' ,7.49,1.5,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Gay Lea Sour Cream 250 ml','250ml($1.00 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/6f3a58d4-6dc1-43b0-9748-0d599c7e2888/300x300.jpg' ,2.50,2.89,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Silk Dairy-Free Almond Beverage Unsweetened','1.89L($0.21 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/d77129af-a203-4373-b165-be8284479656/300x300.jpg' ,3.99,4.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Compliments Sour Cream 500 ml','500ml($0.57 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/d76fa55d-7208-41ff-8097-2e2715c034ca/300x300.jpg' ,2.84,3.79,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Gay Lea Low Fat Sour Cream 250 ml','250ml($1.00 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/bc938c31-7099-4879-b172-7909024569b2/300x300.jpg' ,2.50,2.89,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy', 'Silk Dairy-Free Beverage Almond Original 1.89 L','1.89L($0.21 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/c829c7c6-e6e3-4d7c-8b50-045b75d345e5/300x300.jpg' ,3.99,4.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Activia Yogurt with Probiotics Vanilla Flavour 650 g','650g($0.61 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/4c9c2163-3105-4666-bb31-2000233430e5/300x300.jpg' ,3.99,4.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy',  'Liberté Classique 2.9% Yogurt Strawberry 650 g','650g($0.46 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/dc50f842-4912-40cf-819a-e781d6449d72/300x300.jpg' ,3.00,4.49,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy', 'Becel Unsalted Plant-Based Margarine Bricks 454 g', '454g($1.10 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/b84a3d49-39a6-4950-b716-d8d21a721546/300x300.jpg' ,4.99,6.29,5);
+
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Silk Gluten-Free Beverage Almond Dark Chocolate 1.89 L','1.89L($0.21 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/b499252f-6615-48c1-8e80-5fd104e6fae7/300x300.jpg' ,3.99,4.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Activia Yogurt with Probiotics Vanilla Flavour 100 g (Pack of','8 x 100g($0.62 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/79328241-ec87-4fd5-8738-8e3e03079077/300x300.jpg' ,4.99,6.49,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','I Cant Believe Its Not Butter Margarine 454 g','427g($0.59 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/6fd3076b-59d1-472f-8b8a-fa3774f40c3e/300x300.jpg' ,2.50,3.69,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Activia Yogurt with Probiotics Raspberry Flavour 650 g','650g($0.61 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/1304e599-50e7-4944-bea4-e7f658b2cddf/300x300.jpg' ,3.99,4.99,5);
+
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Gray Ridge Premium Grade A Eggs White Large 12 Count','12 per pack($0.39 per item)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/072189a3-5250-4569-a449-03a32bf9b28a/300x300.jpg' ,4.69,6.89,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Gray Ridge Grade A Brown Eggs Large 18 Count','18 per pack($0.38 per item)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/ddcbb1b6-e7cf-4436-b5f0-d3673acf2037/300x300.jpg' ,6.89,8.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Liberté 2% Goat Milk 4 L','','' ,3.99,4.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Liberté 2% Goat Milk 4 L','4L($0.40 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/25d08b36-d92c-42e6-a706-6ddf2db5924f/300x300.jpg' , 15.99,18.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Dairy','Natrel Fine-Filtered 1% Milk 4 L','4L ($0.16 per 100ml)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/f3e82cf6-2cd5-48f9-a2d3-3810ad32e85f/500x500.jpg' ,6.39,9.99,5);
+
+
+-- Frozen Food
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','Delissio Rising Crust Pepperoni 12 Inch Frozen Pizza 788 g','788g($1.18 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/82da050d-e956-46b1-9f33-f762af9b9bff/300x300.jpg' ,9.29,12.54,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','Delissio Rising Crust 4 Cheese Frozen Pizza 782 g','782g($1.19 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/49108da9-dd66-4968-a762-e95b9354df58/300x300.jpg' ,9.29,12.54,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','Johnsonville Frozen Sausage Breakfast Original 375 g','375g($1.60 per 100g)','https://voila.ca/images-v3/2d92d19c-0354-49c0-8a91-5260ed0bf531/3e4b45e4-5941-484c-930a-2926b0cb43a6/300x300.jpg' ,5.99,6.49,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+
+
+
+INSERT INTO products (category_name_value,product_name,product_description,product_url,price, sale_price,quantity) VALUES ('Frozen Fodd','','','' ,6.39,9.99,5);
+
