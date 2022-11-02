6ad9bc6cf86a2d805876b7bdaa4b1ea5bc52ba01
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-01 21:00:45 -0400

    fixing the database structure

--- server/db/schema/04_create_products.sql
+++ server/db/schema/04_create_products.sql
@@ -3,7 +3,9 @@ CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   -- store_id integer REFERENCES stores(id) ON DELETE CASCADE NOT NULL,
   product_name VARCHAR(255) NOT NULL,
-  price INTEGER, 
+  product_description VARCHAR(255) NOT NULL,
+  price FLOAT, 
+  sale_price FLOAT,
   quantity INTEGER
   -- catergories table, fruits, vegatbales, dairy, price change the interger to a float ,
   -- Images for products, description of product, image URL status on product true or false, to show or not to show
--- server/db/seeds/04_products.sql
+++ server/db/seeds/04_products.sql
@@ -1,5 +1,5 @@
-INSERT INTO products (product_name,price, quantity) VALUES ( 'egg', 3,5);
-INSERT INTO products (product_name, price, quantity) VALUES ('milk', 10,6);
-INSERT INTO products (product_name,price, quantity) VALUES ('yogurt', 20,7);
-INSERT INTO products (product_name, price, quantity) VALUES ('cucumber', 5,6);
-INSERT INTO products (product_name, price, quantity) VALUES ('avocado', 9,10);
+INSERT INTO products (product_name,product_description,price, sale_price,quantity) VALUES ( 'egg','egg' ,3.24234,5.23423,5);
+-- INSERT INTO products (product_name, price, quantity) VALUES ('milk', 10,6);
+-- INSERT INTO products (product_name,price, quantity) VALUES ('yogurt', 20,7);
+-- INSERT INTO products (product_name, price, quantity) VALUES ('cucumber', 5,6);
+-- INSERT INTO products (product_name, price, quantity) VALUES ('avocado', 9,10);
