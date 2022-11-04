8b352d536515ddf43cf7420b583498fe50cd3564
Nabil Ahmed <nabilahmed98@hotmail.com>
2022-11-04 20:09:32 +0000

    moving photos above text

--- client/src/Home.js
+++ client/src/Home.js
@@ -64,6 +64,12 @@ function Home(props) {
                         <Nav />
                         
                     </div>
+                        <div className="sale-photo">
+                            <img className="secoundlogoImage" src="https://image.s11.sfmc-content.com/lib/fe3e157175640479711675/m/10/b7102c92-08fc-4bea-8ee9-0b595a543f04.png" />
+                        <div className="sale-photo">
+                            <img className="secoundlogoImage" src="https://image.s11.sfmc-content.com/lib/fe3e157175640479711675/m/10/404a4ff9-d8c4-4f48-975e-79d421962f47.png" />
+                       </div>
+                        </div>
                     <div className="eachrow">
                         <div className="eachcloumn">
                             <h1>Look no Further!<br /> We got Everything for you.</h1>
@@ -72,12 +78,6 @@ function Home(props) {
                             <a href="" className="firstbutton">CHECK IT OUT &#x2192;</a>
                         </div>
                     </div>
-                        <div className="sale-photo">
-                            <img className="secoundlogoImage" src="https://image.s11.sfmc-content.com/lib/fe3e157175640479711675/m/10/b7102c92-08fc-4bea-8ee9-0b595a543f04.png" />
-                        <div className="sale-photo">
-                            <img className="secoundlogoImage" src="https://image.s11.sfmc-content.com/lib/fe3e157175640479711675/m/10/404a4ff9-d8c4-4f48-975e-79d421962f47.png" />
-                       </div>
-                        </div>
                 </div>
             </header>
             
