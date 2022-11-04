80a068745f96b2c28e2094aea54e3e2c03936a0e
Nabil Ahmed <nabilahmed98@hotmail.com>
2022-11-04 00:17:28 +0000

    about page creation and elementery syling

--- client/src/About.js
+++ client/src/About.js
@@ -1,14 +1,12 @@
 import React from "react";
+import "./About.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-function About (props){
-
-    return (
-        <div>
-            <h1>
-                Welcome to the About Page
-            </h1>
-            <nav className="navlistItems">
-                        <ul>
+function About(props) {
+  return (
+    <div>
+  
+      <nav className="navlistItems">
+        <ul>
           <li>
             <Link to="/home">Home</Link>
           </li>
@@ -24,22 +22,52 @@ function About (props){
           <li>
             <Link to="/about">About</Link>
           </li>
-          
+
           <li>
             <Link to="/main">Main</Link>
           </li>
           <li>
-            <Link to="/cart"><img className="cartIcon" alt="Image cart" src="https://img.icons8.com/parakeet/2x/shopping-cart.png"></img></Link>
+            <Link to="/cart">
+              <img
+                className="cartIcon"
+                alt="Image cart"
+                src="https://img.icons8.com/parakeet/2x/shopping-cart.png"
+              ></img>
+            </Link>
           </li>
           <li>
             <Link to="/summary">Summary Cart</Link>
           </li>
         </ul>
-                         
-                        </nav>
-            
-        </div>
-    )
+      </nav>
+
+      <div className="about">
+        <h1>Welcome to LiteCart!</h1>
+      </div>
+
+      <div className="info">
+        <p>
+          LiteCart is a cutting-edge online grocery home delivery service in the
+          Greater Toronto Area.</p>
+          
+           <p>We have partnered with many of your favourite grocery stores to build automated options that improve
+          the quality of the service experience for Canadians.</p> 
+          
+          <p>At LiteCart, we
+          promise guaranteed freshness, at affordable prices, straight to your
+          door.
+        </p>
+      </div>
+
+      <div className="list">
+        <h1>
+          The LiteCart Difference.
+        </h1>
+      </div>
+
+      
+    </div>
+  );
 }
 
 export default About;
