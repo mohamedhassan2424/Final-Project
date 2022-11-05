780edf7230e9fc8c5f10444ff37bb2b13bc1e9c2
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-05 09:03:08 -0400

    was able to get the join to work for the products and the table

--- server/db/seeds/05_sales.sql
+++ server/db/seeds/05_sales.sql
@@ -6,3 +6,10 @@
 
 -- INSERT INTO sales (user_id_sales,stores_id_sales, products_id ) VALUES (1,1,1);
 -- INSERT INTO sales (user_id_sales,stores_id_sales, products_id ) VALUES (3,6,4);
+
+
+SELECT * FROM sales
+JOIN products ON products.id = products_id
+JOIN users ON users.id = user_id_sales
+JOIN stores ON stores.id = stores_id_sales
+WHERE users.id =10 ;
