2ad3f80a83d468865f16ba04fadb83670f28436e
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-11-08 09:22:54 -0500

    was able to get the redirect page to work know i just need to make the original data in the sales history disappear

--- client/src/Checkout.js
+++ client/src/Checkout.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {loadStripe} from '@stripe/stripe-js';
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 import {
   CardElement,
   Elements,
@@ -10,6 +11,7 @@ import {
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 const Checkout = () => {
+  const history = useHistory();
   const cookies = new Cookies();
 
   const userIdValueNum = cookies.get('userId')
@@ -38,7 +40,8 @@ console.log('salesHistoryTableCookie',salesHistoryIdCookie)
         axios.post('http://localhost:8080/addingToSalesHistory',{userId:eachProductSales.user_id_sales,stores_id:eachProductSales.stores_id_sales,products_id :eachProductSales.products_id,count_product:eachProductSales.count_product})
         .then((response)=>{
          console.log("All is good the data has been sent offf")
-         
+         let path =`/salesHistory`;
+         history.push(path);
         })
       })
       
