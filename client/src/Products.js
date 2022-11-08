b62e64c77b22608dee455e3503c5a9ec78366677
Nabil Ahmed <nabilahmed98@hotmail.com>
2022-11-08 02:00:00 +0000

    banner on products page

--- client/src/Products.js
+++ client/src/Products.js
@@ -59,6 +59,9 @@ function Products(props) {
             <div className="navbar">
                
                <Nav />
+               <div className="banner">
+                            <img className="banner" src="https://voila.ca/assets/content/2d92d19c-0354-49c0-8a91-5260ed0bf531/creatives/4af4835b-aab5-4b75-bd5e-3ed97cf6245d/71c68b3f75.png" />
+                        </div>
             </div>
             <h1 className="dairy">{dairy[0] ? dairy[0].category_name_value:<h1>Waiting .......</h1>}</h1>
             <div className="eachrow">
